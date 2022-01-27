814f5f9ced2fa72e9d88a7beeab79506bfe48a3b
Felipe-ce <felipe_rsr@live.com>
2022-01-27 19:27:42 -0300

    update desafio 04

---
+++ desafio04.js
@@ -0,0 +1,23 @@
+//1 - crie uma função que exiba uma mensagem no console
+function exibirMensagem() {
+  console.log('Exibir função')
+}
+exibirMensagem()
+
+//2 - crie uma função que receba o seu nome como (parâmetro) e exiba no console
+function meuNome(colaborador) {
+  console.log(colaborador)
+}
+meuNome('Felipe')
+
+//3 - crie uma função que receba nome, idade, e um estilo musical preferido (parâmetros) e exiba no console
+function perfil(nome, idade, estilo) {
+  console.log(nome, idade, estilo)
+}
+perfil('Felipe', 30, "variados")
+
+//4 - crie uma função que receba um filme, uma música (parâmetros) e exiba no console
+function perfilMusica(filme, musica) {
+  console.log(filme, musica)
+}
+perfilMusica('Titanic',  "Trem das onze")
